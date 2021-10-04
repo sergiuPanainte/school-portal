d5b8a3e87bb7e34647ec01e75eb483b38ff58133
utxmu <utxmu@student.kit.edu>
2021-10-05 00:55:49 +0200

    Create entry point

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require('express');
+const path = require('path');
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "3000";
+
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+/**
+ * Server Activation
+ */
