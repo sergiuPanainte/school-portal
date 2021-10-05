65f1d1f409564c17d8bd446a6440f8d29faa0702
utxmu <utxmu@student.kit.edu>
2021-10-05 15:30:06 +0200

    Render views

--- src/index.js
+++ src/index.js
@@ -9,16 +9,12 @@ const path = require("path");
  * App Variables
  */
 const app = express();
-const port = process.env.PORT || "3000";
+const port = process.env.PORT || "8000";
 
 
 /**
  *  App Configuration
  */
-
-app.get('/', (req, res) => {
-    res.status(200).send('Welcome to School Portal!');
-});
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
@@ -26,6 +22,10 @@ app.set("view engine", "pug");
 /**
  * Routes Definitions
  */
+
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
 
 /**
  * Server Activation
