037ca2dbe4646e5c13077c27e0b3baf558cd0583
utxmu <utxmu@student.kit.edu>
2021-10-18 19:53:43 +0200

    Fix showmore btn

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,14 +4,15 @@ var toCollapse;
 window.onload = function () {
     showmore_btn = document.getElementById('rotateBtn');
     toCollapse = document.getElementsByClassName('UtilContainer')[0];
+    toCollapse.style.display = 'none';
 }
 
 function showMore() {
     if (toCollapse.style.display === 'none') {
-        showmore_btn.style.transform = 'rotate(180deg)'
+        showmore_btn.style.transform = 'rotate(180deg)';
         toCollapse.style.display = 'grid';
         } else {
-            showmore_btn.style.transform = 'rotate(180deg)'
+            showmore_btn.style.transform = 'rotate(0)';
             toCollapse.style.display = 'none';
         }
 }
